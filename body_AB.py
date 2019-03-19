ee2c838f3588ff8fee90a90ba31a29ec1f0e9649
AndreaBarbon <andrea.3arbon@gmail.com>
2019-03-19 10:07:50 -0400

    cuda?

--- body_AB.py
+++ body_AB.py
@@ -19,6 +19,9 @@ from custom_types import DaRnnNet, TrainData, TrainConfig
 from utils import numpy_to_tvar
 from constants import device
 
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 logger = utils.setup_log()
 logger.info(f"Using computation device: {device}")
 
